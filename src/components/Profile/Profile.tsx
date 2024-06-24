7b7d8d8bf223cdd2c750db9dc98e9d9fda6c5091
DeHopen <a2302004b23@yandex.ru>
2024-06-24 17:35:09 +0300

    FixBug

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -38,6 +38,24 @@ const Profile: React.FC = () => {
     }
   }, []);
 
+  const defaultAge = 'Возраст не указан';
+  const defaultReligion = 'Религия не указана';
+
+  const renderAgeReligion = () => {
+    const age = profile.age || defaultAge;
+    const religion = profile.religion || defaultReligion;
+
+    if (!profile.age && !profile.religion) {
+      return <p className={styles['age']}>0 лет</p>;
+    }
+
+    if (profile.age && !profile.religion) {
+      return <p className={styles['age']}>{age} лет</p>;
+    }
+
+    return <p className={styles['age']}>{age} лет | {religion}</p>;
+  };
+
   return (
       <div className={styles.profile}>
         <div className={styles['header-container']}>
@@ -51,8 +69,12 @@ const Profile: React.FC = () => {
           </div>
           <CarouselElement/>
         </div>
-        <h1>{profile.name}</h1>
-        <p className={styles['age']}>{profile.age} лет | {profile.religion}</p>
+        { profile.name ? (
+            <h1>{profile.name}</h1>
+        ) : (
+            <h1>Имя</h1>
+        )}
+        {renderAgeReligion()}
         { profile.about && (
             <div className={styles["about"]}>
               <h2>О себе</h2>
